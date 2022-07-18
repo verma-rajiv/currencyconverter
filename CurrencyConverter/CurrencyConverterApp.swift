093764fb8e0f39611a49ca766530ad617160529c
Rajiv <rajiv.verma@publicissapient.com>
2022-07-18 13:45:02 +0530

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Rajiv Verma on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
